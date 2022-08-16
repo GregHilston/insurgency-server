05068c66d0ac6e100a4f1252fbdbfd9ee4b48098
GregHilston <Gregory.Hilston@gmail.com>
2022-08-15 22:06:19 -0400

    fix: bash script by instantiating task var and removing premature exit

--- bin/server-start.sh
+++ bin/server-start.sh
@@ -7,19 +7,18 @@ IFS=$'\n\t'
 source "./.env"
 
 # --raw ensures that the terraform output is not wrapped in quotes for string representation
-cluster_name=$(bin/terraform output --raw cluster_name)
-service_name=$(bin/terraform output --raw service_name)
+cluster_name=$(./bin/terraform.sh output --raw cluster_name)
+service_name=$(./bin/terraform.sh output --raw service_name)
 
 # Scale the service up to get a task running
 aws ecs update-service --cluster $cluster_name --service $service_name \
-    --desired-count 1 #> /dev/null
-
-exit 1
+    --desired-count 1 > /dev/null
 
 # Poll ECS to get the running task ARN
+task=""
 while [ "$task" = "" ]; do
-    task=$(aws ecs list-tasks --cluster "$cluster_name" \
-        --service-name "$service_name" | jq -r '.taskArns[]')
+    task=$(aws ecs list-tasks --cluster $cluster_name \
+        --service-name $service_name | jq -r '.taskArns[]')
 
     test "$task" = "" && sleep 6
 done
@@ -28,15 +27,15 @@ done
 aws ecs wait tasks-running --cluster "$cluster_name" --tasks "$task"
 
 # Describe the running task
-network_interface=$(aws ecs describe-tasks --cluster "$cluster_name" \
---tasks "$task" | jq -r '.tasks[].attachments[] | 
-  select(.type == "ElasticNetworkInterface")
-  .details[] | select(.name == "networkInterfaceId") .value')
+network_interface=$(aws ecs describe-tasks --cluster $cluster_name \
+    --tasks "$task" | jq -r '.tasks[].attachments[] |
+    select(.type == "ElasticNetworkInterface")
+    .details[] | select(.name == "networkInterfaceId") .value')
 
 # Get the IP address from this network interface
 public_ip=$(aws ec2 describe-network-interfaces  \
---filters Name=network-interface-id,Values="$network_interface" | \
-jq -r '.NetworkInterfaces[].Association.PublicIp')
+    --filters Name=network-interface-id,Values="$network_interface" | \
+    jq -r '.NetworkInterfaces[].Association.PublicIp')
 
 echo "Server started successfully"
 echo "Public IP address: $public_ip"
--- bin/server-stop.sh
+++ bin/server-stop.sh
@@ -4,11 +4,11 @@
 # shellcheck disable=2039 disable=1091
 source "./.env"
 
-cluster_name=$(bin/terraform output cluster_name)
-service_name=$(bin/terraform output service_name)
+cluster_name=$(bin/terraform.sh output --raw cluster_name)
+service_name=$(bin/terraform.sh output --raw service_name)
 
 # Scale down so we stop the service
-aws ecs update-service --cluster "$cluster_name" --service "$service_name" \
+aws ecs update-service --cluster $cluster_name --service $service_name \
     --desired-count 0 > /dev/null
 
 echo "Server is being stopped"
